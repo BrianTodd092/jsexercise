0bc0f95c58562a8ce66ccc82e941d277857b6656
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2022-10-15 13:43:36 +0100

    Add files via upload

---
+++ scripta.js
@@ -0,0 +1,27 @@
+// reference error press F12 select console,console.log(x),
+
+
+var x = 10;      //declare variable x & assign value 10.
+//x= 100;          // re assign value to x = 100;
+//var x = "re declared";
+
+let y = "red";                // declare y
+//y = "purple";                //reassign y
+//let y ="orange";        // cannot redeclare y or let x as var x.
+
+const car ="volvo";    // declared const cannot redeclare or reassign.
+//const car = "fiat";    const has already been declared error.
+
+var p;
+if ( p === undefined){
+
+alert("var p undefined");
+
+}
+
+console.log(x);
+console.log(y);
+console.log(car);
+console.log(z); // z not defined, not mentioned.
+console.log(p);
+
